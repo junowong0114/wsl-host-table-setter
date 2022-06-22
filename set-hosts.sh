2e30d5dcf6575068a054b4ae0299587c6fc3c933
Juno Wong, CL (ITS) <juno.cl.wong@hktdc.org>
2022-06-22 09:53:39 +0800

    add set list function

--- set-hosts.sh
+++ set-hosts.sh
@@ -2,14 +2,6 @@
 set -e
 set -o pipefail
 
-usage() {
-    echo "Usage:"
-    printf "hosts.sh [-f CONFIG_FILE] \n\n"
-    echo "Description:"
-    echo "-f, CONFIG_FILE       The path of hosts.yml [default \"~/.hosts/hosts.yml\"]"
-    exit -1
-}
-
 # useful constants
 CONFIG_FILE="$HOME/.hosts/hosts.yml"
 HOST_TABLE_FILE="/mnt/c/Windows/System32/drivers/etc/hosts"
@@ -17,6 +9,19 @@ HOST_TABLE_FILE="/mnt/c/Windows/System32/drivers/etc/hosts"
 # read hostnames from CONFIG_FILE
 declare -a HOSTNAMES
 HOSTNAMES=($(cat $CONFIG_FILE | grep -e '^[^ ].*' | perl -pe "s|(.*):|\1|"))
+
+usage() {
+    echo "Usage:"
+    echo "  set                           Set the host table"
+    echo "  edit config                   Edit the config file [default: $HOME/.hosts/hosts.yml]"
+    echo "  edit table                    Edit the host table directly [default: /mnt/c/Windows/System32/drivers/etc/hosts]"
+    echo "  list                          List the hosts table settings"
+    printf "\n"
+    echo "Options:"
+    echo "  -f, CONFIG_FILE               The path of hosts.yml [default \"~/.hosts/hosts.yml\"]"
+    echo "  -h                            Display the help menu"
+    exit -1
+}
 
 list() {
     set +e
@@ -38,8 +43,69 @@ list() {
     exit 0 
 }
 
-while getopts ':f:hl' OPT; do
-  case $OPT in
+set_host() {
+    echo "Select a hostname to be configured:"
+    declare -a OPTIONS
+    OPTIONS+=("Disable host table setting")
+    ACTIVE=false
+
+    while [[ ! $TARGET_HOSTNAME ]]; do
+        select TARGET_HOSTNAME in ${HOSTNAMES[@]}; do
+            if [[ ! $TARGET_HOSTNAME ]]; then
+                printf "\nInvalid selection, try again:\n"
+                break
+            fi
+
+            while read line; do
+                if [[ $ACTIVE == 'true' ]]; then
+                    if [[ ! $line =~ ^-.*$ ]]; then
+                        break
+                    fi;
+
+                    desc=$(echo $line | perl -pe "s|- [\"']*(.*):((?:[0-9]+\.){3}[0-9]+)[\"']*|\1|")
+                    ip=$(echo $line | perl -pe "s|- [\"']*(.*):((?:[0-9]+\.){3}[0-9]+)[\"']*|\2|")
+
+                    OPTIONS+=("$ip ($desc)")
+                fi
+
+                if [[ $line == $TARGET_HOSTNAME: ]]; then
+                    ACTIVE=true
+                fi
+            done < $CONFIG_FILE
+            break
+        done
+    done
+
+    printf "\nSelect an ip to be set to $TARGET_HOSTNAME:\n"
+
+    while [[ ! $OPTION ]]; do
+        select OPTION in "${OPTIONS[@]}"; do
+            if [[ ! $OPTION ]]; then
+                printf "\nInvalid selection, try again:\n"
+                break
+            fi
+
+            if [[ $OPTION == 'Disable host table setting' ]]; then
+                sed -i "/.*$TARGET_HOSTNAME.*/d" $HOST_TABLE_FILE
+                printf "\nDisabled host table settings to $TARGET_HOSTNAME.\n"
+                break
+            fi
+
+            IP=$(echo $OPTION | perl -pe "s|((?:[0-9]+\.){3}[0-9]+).*|\1|")
+            if [[ $(cat $HOST_TABLE_FILE | grep $TARGET_HOSTNAME) ]]; then
+                perl -i -pe "s|.*$TARGET_HOSTNAME.*|$IP $TARGET_HOSTNAME|" $HOST_TABLE_FILE
+            else
+                echo "$IP $TARGET_HOSTNAME" >> $HOST_TABLE_FILE
+            fi;
+            printf "\nSuccessfully set $TARGET_HOSTNAME to $IP.\n"
+            break
+        done
+    done
+}
+
+# get options
+while getopts ':f:h' OPT; do
+case $OPT in
     f)
         CONFIG_FILE_STRING=$OPTARG
         if [[ $CONFIG_FILE_STRING =~ ^~.*$ ]]; then
@@ -49,8 +115,6 @@ while getopts ':f:hl' OPT; do
         HOSTNAMES=($(cat $CONFIG_FILE | grep -e '^[^ ].*' | perl -pe "s|(.*):|\1|"));;
     h) 
         usage;;
-    l) 
-        list;;
     \:) 
         printf "Error: Argument missing from -%s option\n\n" $OPTARG
         usage
@@ -60,64 +124,34 @@ while getopts ':f:hl' OPT; do
         usage
         exit 2
         ;;
-  esac >&2
+esac >&2
 done
-shift $(($OPTIND - 1))
+shift "$(($OPTIND - 1))"
 
-echo "Select a hostname to be configured:"
-declare -a OPTIONS
-OPTIONS+=("Disable host table setting")
-ACTIVE=false
+if [[ $1 == "edit" ]]; then
+    if [[ $2 == "config" ]]; then
+        nano $CONFIG_FILE
+        exit 0
+    elif [[ $2 == "table" ]]; then
+        nano $HOST_TABLE_FILE
+        exit 0
+    else
+        usage
+        exit 0
+    fi
+fi
 
-while [[ ! $TARGET_HOSTNAME ]]; do
-    select TARGET_HOSTNAME in ${HOSTNAMES[@]}; do
-        if [[ ! $TARGET_HOSTNAME ]]; then
-            printf "\nInvalid selection, try again:\n"
-            break
-        fi
+if [[ $1 == "set" ]]; then
+    set_host
+    exit 0
+fi
 
-        while read line; do
-            if [[ $ACTIVE == 'true' ]]; then
-                if [[ ! $line =~ ^-.*$ ]]; then
-                    break
-                fi;
+if [[ $1 == "list" ]]; then
+    list
+    exit 0
+fi
 
-                desc=$(echo $line | perl -pe "s|- [\"']*(.*):((?:[0-9]+\.){3}[0-9]+)[\"']*|\1|")
-                ip=$(echo $line | perl -pe "s|- [\"']*(.*):((?:[0-9]+\.){3}[0-9]+)[\"']*|\2|")
-
-                OPTIONS+=("$ip ($desc)")
-            fi
-
-            if [[ $line == $TARGET_HOSTNAME: ]]; then
-                ACTIVE=true
-            fi
-        done < $CONFIG_FILE
-        break
-    done
-done
-
-printf "\nSelect an ip to be set to $TARGET_HOSTNAME:\n"
-
-while [[ ! $OPTION ]]; do
-    select OPTION in "${OPTIONS[@]}"; do
-        if [[ ! $OPTION ]]; then
-            printf "\nInvalid selection, try again:\n"
-            break
-        fi
-
-        if [[ $OPTION == 'Disable host table setting' ]]; then
-            sed -i "/.*$TARGET_HOSTNAME.*/d" $HOST_TABLE_FILE
-            printf "\nDisabled host table settings to $TARGET_HOSTNAME.\n"
-            break
-        fi
-
-        IP=$(echo $OPTION | perl -pe "s|((?:[0-9]+\.){3}[0-9]+).*|\1|")
-        if [[ $(cat $HOST_TABLE_FILE | grep $TARGET_HOSTNAME) ]]; then
-            perl -i -pe "s|.*$TARGET_HOSTNAME.*|$IP $TARGET_HOSTNAME|" $HOST_TABLE_FILE
-        else
-            echo "$IP $TARGET_HOSTNAME" >> $HOST_TABLE_FILE
-        fi;
-        printf "\nSuccessfully set $TARGET_HOSTNAME to $IP.\n"
-        break
-    done
-done
+if [[ $1 == "" ]]; then
+    usage
+    exit 0
+fi
